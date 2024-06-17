5f0d1dee12c7f81bdecedff8776970a026d7582c
hyejin <hyejda00@naver.com>
2024-06-18 02:14:12 +0900

    프로젝트 시작

---
+++ src/main/java/com/sparta/streaming_practice/StreamingPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.streaming_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingPracticeApplication.class, args);
+	}
+
+}
