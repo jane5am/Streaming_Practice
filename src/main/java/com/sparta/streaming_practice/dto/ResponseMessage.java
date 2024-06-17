3f1b83273fec383191004c6d70e88e3b606f7089
hyejin <hyejda00@naver.com>
2024-06-18 04:02:51 +0900

    Feat: ResponseMessage 코드 작성

---
+++ src/main/java/com/sparta/streaming_practice/dto/ResponseMessage.java
@@ -0,0 +1,18 @@
+package com.sparta.streaming_practice.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+public class ResponseMessage {
+    private Object data;
+
+    private int statusCode;
+
+    private String resultMessage;
+
+    private String detailMessage;
+}
